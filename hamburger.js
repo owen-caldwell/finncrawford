7db6b8b1fd30107e87d56460c99ec8cb56069e81
owen-caldwell <caldwellom@gmail.com>
2023-08-18 09:34:40 -0400

    latest aug 16

--- hamburger.js
+++ hamburger.js
@@ -1,10 +1,9 @@
 let tg = true;
-const menubtn = document.getElementById("menubtn");
+const menubtn = document.querySelector("#menubtn");
 const nav = document.querySelector("nav");
 console.log("Hi! I'm running for the hamburger menu.");
 function toggleNav() {
     if (tg) {
-        console.log("Nav opening.");
         tg = false;
         nav.style.display = "block";
         menubtn.textContent = "(x)";
@@ -12,7 +11,6 @@ function toggleNav() {
         
     }
     else {
-        console.log("Nav closing.");
         tg = true;
         nav.style.display = "none";
         menubtn.textContent = "(menu)";
