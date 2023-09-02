c75a30db04f3998efa65ade10e796d866f78207d
Owen Caldwell <77856442+owen-caldwell@users.noreply.github.com>
2023-09-02 16:26:00 -0400

    Redesign started sept 2nd

--- hamburger.js
+++ hamburger.js
@@ -1,22 +1,20 @@
-let tg = true;
-const menubtn = document.querySelector("#menubtn");
-const nav = document.querySelector("nav");
-console.log("Hi! I'm running for the hamburger menu.");
-function toggleNav() {
-    if (tg) {
-        tg = false;
-        nav.style.display = "block";
-        menubtn.textContent = "(x)";
-        // write code for when user clicks "menu"
-        
+const index = document.querySelector(".index");
+const feature = document.querySelector(".feature-gallery");
+const hamburger = document.querySelector("#hamburger");
+let open = false;
+function toggleIndex() {
+    if (open === false) {
+        open = true;
+        console.log("opening")
+        index.style.display = "flex";
+        feature.style.display = "none";
+        hamburger.innerHTML = "(x)";
+    } else {
+        open = false;
+        console.log("closing")
+        index.style.display = "none";
+        feature.style.display = "flex";
+        hamburger.innerHTML = "(menu)";
     }
-    else {
-        tg = true;
-        nav.style.display = "none";
-        menubtn.textContent = "(menu)";
-        // write code for close button
-    };
-    
-};
-
-menubtn.addEventListener("click", toggleNav);
+}
+hamburger.addEventListener("click", toggleIndex);
