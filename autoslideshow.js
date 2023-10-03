380c7ba4ecb4922f17a4a204ddee5b1c51684cc7
owen-caldwell <caldwellom@gmail.com>
2023-10-03 14:54:58 -0400

    latest oct 3

--- autoslideshow.js
+++ autoslideshow.js
@@ -1,9 +1,6 @@
 let slideIndex = 0;
 const dnum = document.querySelector(".dnum");
 const dtotal = document.querySelector(".dtotal");
-const mnum = document.querySelector(".mnum");
-const mtotal = document.querySelector(".mtotal");
-const mtitle = document.querySelector(".mobile-title");
 const dtitle = document.querySelector(".title");
 showSlides();
 
@@ -20,24 +17,18 @@ function showSlides() {
   }
   slides[slideIndex - 1].style.display = "flex";
   dtotal.innerHTML = "0" + slides.length;
-  mtotal.innerHTML = "0" + slides.length;
   dnum.innerHTML = "0" + slideIndex;
-  mnum.innerHTML = "0" + slideIndex;
 
   if (slides[slideIndex - 1].className === "abstract") {
-    mtitle.innerHTML = "ABSTRACT";
     dtitle.innerHTML = "ABSTRACT";
     slides[slideIndex - 1].setAttribute('href', 'abstract')
-  } else if (slides[slideIndex - 1].className === "mixed") {
-    mtitle.innerHTML = "MIXED";
-    dtitle.innerHTML = "MIXED";
-    slides[slideIndex - 1].setAttribute('href', 'mixed')
+  } else if (slides[slideIndex - 1].className === "fashion") {
+    dtitle.innerHTML = "FASHION";
+    slides[slideIndex - 1].setAttribute('href', 'fashion')
   } else if (slides[slideIndex - 1].className === "people") {
-    mtitle.innerHTML = "PEOPLE";
     dtitle.innerHTML = "PEOPLE";
     slides[slideIndex - 1].setAttribute('href', 'people')
   } else {
-    mtitle.innerHTML = "UNTITLED";
     dtitle.innerHTML = "UNTITLED";
     slides[slideIndex - 1].setAttribute('href', '')
   }
