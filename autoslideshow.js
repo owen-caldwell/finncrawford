a393018ac318ad74e7ec06ec158e4dfb8885d02e
Owen Caldwell <77856442+owen-caldwell@users.noreply.github.com>
2023-10-04 11:59:06 -0400

    adding images pt2

--- autoslideshow.js
+++ autoslideshow.js
@@ -18,19 +18,5 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "flex";
   dtotal.innerHTML = "0" + slides.length;
   dnum.innerHTML = "0" + slideIndex;
-
-  if (slides[slideIndex - 1].className === "abstract") {
-    dtitle.innerHTML = "ABSTRACT";
-    slides[slideIndex - 1].setAttribute('href', 'abstract')
-  } else if (slides[slideIndex - 1].className === "fashion") {
-    dtitle.innerHTML = "FASHION";
-    slides[slideIndex - 1].setAttribute('href', 'fashion')
-  } else if (slides[slideIndex - 1].className === "people") {
-    dtitle.innerHTML = "PEOPLE";
-    slides[slideIndex - 1].setAttribute('href', 'people')
-  } else {
-    dtitle.innerHTML = "UNTITLED";
-    slides[slideIndex - 1].setAttribute('href', '')
-  }
   setTimeout(showSlides, 5000);
 }
